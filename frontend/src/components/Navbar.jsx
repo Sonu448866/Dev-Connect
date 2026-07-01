3620a955bffb35864286511927b3c92e7bf6123c
Sonu Kumar <kumarsonu448866@gmail.com>
2026-07-01 18:46:22 +0530

    updated app name

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
       <div className="navbar bg-base-300 shadow-sm">
         <div className="flex-1">
           <Link to={"/"} className="btn btn-ghost text-xl">
-            devTinder
+            DevConnect
           </Link>
         </div>
         <div className="flex gap-2">
